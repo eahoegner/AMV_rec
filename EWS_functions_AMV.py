e2a470c11165232aa478fbf74dee14c55854fd9e
eahoegner <129191114+eahoegner@users.noreply.github.com>
2023-03-31 00:38:06 +0200

    Python code with subroutines for the EWS analysis

--- EWS_functions_AMV.py
+++ EWS_functions_AMV.py
@@ -1,3 +1,5 @@
+# adapted from code provided by Vanessa Skiba
+
 import pandas as pd
 import numpy as np
 import scipy.stats as st
@@ -5,22 +7,36 @@ from scipy.optimize import curve_fit
 
 
 def funcfit2(x, a, b, c):
+    """ 
+    model function for curve fit
+    """
     if b <= 0 or c <= 0:
         return 1e8
     else:
         return a + np.power(-b * (x - c), 1 / 2)
 
 def funcfit3(x, a, b, c):
+    """ 
+    model function for curve fit
+    """
     if b <= 0 or c <= 0:
         return 1e8
     else:
         return a + np.power(-b * (x - c), 1 / 3)
 
 def funcfit3_jac(x, a, b, c):
+    """
+    jacobian of model function
+    """
     return np.array([np.ones(x.shape[0]), -(x-c) * np.power(-b * (x - c), -2/3) / 3, b * np.power(-b * (x - c), -2/3) / 3]).T
 
 
 def standardise(x, time):
+    """
+    input: dataframe of time series
+    removes trend obtained via non-linear least squares fit
+    standardises to mean 0 and std 1
+    """
     values = []
     for column in x.columns:
         data = x[column]
@@ -32,6 +48,11 @@ def standardise(x, time):
 
 
 def standardise2(x, time):
+    """
+    input: time series
+    removes trend obtained via non-linear least squares fit
+    standardises to mean 0 and std 1
+    """
     popt31, cov = curve_fit(funcfit3, time[time > time[0]], x[time > time[0]], p0 = [-1,  .1,  2030], maxfev = 1000000000, jac = funcfit3_jac)
     detrended = x - funcfit3(time, *popt31)
     standardised = (detrended-detrended.mean())/detrended.std()
@@ -39,22 +60,40 @@ def standardise2(x, time):
 
 
 def fourier_surrogates(ts, ns):
+    """
+    computes Fourier surrogates of a time series
+    by phase randomisation in frequency domain
+    """
+    # Fourier transform
     ts_fourier  = np.fft.rfft(ts)
+    # phase randomisation
     random_phases = np.exp(np.random.uniform(0, 2 * np.pi, (ns, ts.shape[0] // 2 + 1)) * 1.0j)
     ts_fourier_new = ts_fourier * random_phases
+    # inverse Fourier transform
     new_ts = np.real(np.fft.irfft(ts_fourier_new))
     return new_ts
 
 
 def kendall_tau_test(ts, ns, w, tau, EWS, bound, mode1 = 'fourier', mode2 = 'linear'):
+    """
+    significance test of EWS against Fourier surrogates
     
-    # ts is the detrended time series
-    # ns is the number of fourier surrogates sampled
-    # w is the window size
-    # tau is the slope of the fit
-    # EWS is the kind of EWS we are looking at
-    # bound is the bounds of the time series
-    
+    Parameters
+        ----------
+        ts: detrended time series         
+        ns: number of Fourier surrogates
+        w: window size
+        tau: slope of the linear fit
+        EWS: EWS used for analysis
+             var: variance
+             ar: lag-1 autocorrelation
+             lam: restoring rate (lambda)
+        bound: cutoff at the ends of the time series, f.ex. w/2 to ensure constant window size
+            
+    Returns
+    -------
+    p-value
+    """
     tlen = ts.shape[0]
     global surrogate
     if mode1 == 'fourier':
@@ -80,19 +119,22 @@ def kendall_tau_test(ts, ns, w, tau, EWS, bound, mode1 = 'fourier', mode2 = 'lin
     elif mode2 == 'kt':
         for i in range(ns):
             stat[i] = st.kendalltau(np.arange(tlen), surrogate)[0]
+            
+    # distinguish positive and negative trends
     if tau > 0:
         p = 1 - st.percentileofscore(stat, tau) / 100.
     else:
         p = st.percentileofscore(stat, tau) / 100.
-        
-    # returns 1 - percentile of the results of stat that are smaller than tau (the slope of the fit)
-    # i.e. 
     
     return p
 
 
 
 def runstd(x, w):
+    """
+    computes running standard deviation of time series
+    w: window size
+    """
    n = x.shape[0]
    bounds = w // 2
    xs = np.zeros_like(x)
@@ -139,6 +181,10 @@ def runstd(x, w):
 
 
 def runac(x, w):
+    """
+    computes running lag-1 autocorrelation of time series
+    w: window size
+    """
    n = x.shape[0]
    bounds = w // 2
    xs = np.zeros_like(x)
